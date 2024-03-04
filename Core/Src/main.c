dac3cdae356b9fba5979dfc97894be964f8eb2c3
Redes <brunacardoso@alunos.utfpr.edu.br.com>
2024-03-04 15:43:35 -0300

    Testezinnn

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+ff
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
